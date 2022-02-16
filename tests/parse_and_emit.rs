299e673443cf74c837b153d2eb949bead7191ef6
jprochazk <1665677+jprochazk@users.noreply.github.com>
2022-02-16 15:21:10 +0100

    integration test

---
+++ tests/parse_and_emit.rs
@@ -0,0 +1,23 @@
+macro_rules! try_parse_and_emit {
+  ($test_name:ident, $name:literal) => {
+    #[test]
+    fn $test_name() {
+      use gen::ast::AsAst;
+      let oapi =
+        serde_json::from_str::<openapiv3::OpenAPI>(include_str!($name))
+          .unwrap();
+      match oapi.as_ast() {
+        Ok(v) => {
+          println!("{}", gen::emit::emit(v));
+        }
+        Err((v, e)) => {
+          println!("{v:#?}");
+          eprintln!("{e:#?}");
+          panic!("Errors were not empty");
+        }
+      }
+    }
+  };
+}
+
+try_parse_and_emit!(job_queue, "./data/job-queue.json");
